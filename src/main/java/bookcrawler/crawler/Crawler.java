f45cbd707fd0eb40de4e07c58a15b691c18c70d4
abcdefg <choiyurim@naver.com>
2020-06-23 20:16:51 +0900

    크롤러 테스트

--- src/main/java/bookcrawler/crawler/Crawler.java
+++ src/main/java/bookcrawler/crawler/Crawler.java
@@ -1,21 +1,65 @@
 package bookcrawler.crawler;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.parser.ParseData;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 import edu.uci.ics.crawler4j.url.WebURL;
 
+@Component
 public class Crawler extends WebCrawler {
 
 	@Override
 	public void visit(Page page) {
 		// TODO Auto-generated method stub
-		super.visit(page);
+
+		HtmlParseData parseData = (HtmlParseData) page.getParseData();
+
+		String html = parseData.getHtml();
+
+		Document doc = Jsoup.parse(html);
+
+		String content = doc.select("a.title").html();
+
+		System.out.println(content);
+
+		// super.visit(page);
 	}
-	
+
 	@Override
 	public boolean shouldVisit(Page referringPage, WebURL url) {
 		// TODO Auto-generated method stub
-		return super.shouldVisit(referringPage, url);
+		// return super.shouldVisit(referringPage, url);
+
+		return url.getURL().startsWith("https://novel.munpia.com/");
 	}
 
+	
+	public void run(String url) throws Exception {
+		int numberOfCrawlers = 1;
+
+		CrawlConfig config = new CrawlConfig();
+		config.setMaxDepthOfCrawling(1);
+		config.setPolitenessDelay(5000);
+		config.setCrawlStorageFolder("data/crawl");
+
+		PageFetcher pageFetcher = new PageFetcher(config);
+		RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+		RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+
+		CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+		controller.addSeed("https://novel.munpia.com/page/novelous/group/pl.serial/gpage/1");
+
+		controller.start(Crawler.class, numberOfCrawlers);
+	}
 }
--- src/main/java/bookcrawler/crawler/CrawlerRunner.java
+++ src/main/java/bookcrawler/crawler/CrawlerRunner.java
@@ -2,21 +2,26 @@ package bookcrawler.crawler;
 
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
 
 import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
 import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+@Component
 public class CrawlerRunner implements ApplicationRunner {
 
+	private final Crawler crawler;
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		// TODO Auto-generated method stub
-		CrawlConfig config = new CrawlConfig();
-		config.setMaxDepthOfCrawling(1);
-		config.setPolitenessDelay(500);
-		config.setCrawlStorageFolder("data/crawl");
 		
-		PageFetcher pageFetcher = new PageFetcher(config);
+		crawler.run("");
 	}
 
 }
